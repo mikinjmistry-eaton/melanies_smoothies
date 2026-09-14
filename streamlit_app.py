18ba1b988a3e69a40b81a43062d02a45302dec35
mikinjmistry-eaton <mikinjcmistry@eaton.com>
2026-09-14 09:49:56 +0530

    Create streamlit_app.py
    copied from my custom smoothies SIS app

---
+++ streamlit_app.py
@@ -0,0 +1,48 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session
+
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!"""
+)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+name_on_order = st.text_input('Name on Smoothie:')
+
+st.write('The name on your Smoothie will be:', name_on_order)
+
+my_dataframe = session.table("smoothies.public.fruit_options")
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:",
+    my_dataframe.select("FRUIT_NAME").to_pandas()["FRUIT_NAME"].tolist(),
+)
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+
+    my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+
+    time_to_insert = st.button("Submit Order")
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display smoothiefroot nutrition information
+
+# import requests
+
+# smoothiefroot_response = requests.get(
+#     "https://my.smoothiefroot.com/api/fruit/watermelon"
+# )
+
+# st.text(smoothiefroot_response)
