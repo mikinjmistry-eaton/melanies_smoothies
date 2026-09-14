1181de58cfc3889d6470538d1d7f2c2081b6005d
mikinjmistry-eaton <mikinjcmistry@eaton.com>
2026-09-14 10:26:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,6 @@ smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
 
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_contaniner_width=True)
