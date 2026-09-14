47a884fa63ff798da5fdda9527157d95794e1887
mikinjmistry-eaton <mikinjcmistry@eaton.com>
2026-09-14 10:32:12 +0530

    Integrate smoothie fruit nutrition display
    Refactor code to display smoothie fruit nutrition information based on selected ingredients.

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,17 +38,27 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display smoothiefroot nutrition information
+# smoothiefroot_response = requests.get(
+#     "https://my.smoothiefroot.com/api/fruit/watermelon"
+# )
+
+# sf_df = st.dataframe(
+#     data=smoothiefroot_response.json(),
+#     use_container_width=True
+# )
 
 
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
+if ingredients_list:
+    ingredients_string = ''
 
-# st.text(smoothiefroot_response.json())
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
 
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/watermelon"
+        )
 
-sf_df = st.dataframe(
-    data=smoothiefroot_response.json(),
-    use_container_width=True
-)
+        sf_df = st.dataframe(
+            data=smoothiefroot_response.json(),
+            use_container_width=True
+        )
